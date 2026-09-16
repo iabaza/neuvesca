6c3782dc685695aa26a8a8eb89cb10e1576e709a
iabaza <islamabaza2002@gmail.com>
2026-09-16 04:50:37 +0300

    Revert "Add Google Tag Manager (GTM-W5TTK2PF)"
    This reverts commit 3b3dc9e95c4085cc32a29aca0695d659eeeb3e4d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import { createClient } from "@/lib/supabase/server";
 import SiteHeader from "@/components/SiteHeader";
 import SiteFooter from "@/components/SiteFooter";
 import MetaPixel from "@/components/MetaPixel";
-import GoogleTagManager, {
-  GoogleTagManagerNoscript,
-} from "@/components/GoogleTagManager";
 import { CartProvider } from "@/lib/cart/CartProvider";
 import "./globals.css";
 
@@ -43,11 +40,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${serif.variable} ${sans.variable}`}>
-      <head>
-        <GoogleTagManager />
-      </head>
       <body>
-        <GoogleTagManagerNoscript />
         <MetaPixel />
         <CartProvider
           initialIsAuthenticated={Boolean(user)}
